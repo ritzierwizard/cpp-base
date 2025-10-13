ddd77910a727590e59df43a6ce3e0f650eef9d7c
Joshua Antikainen <joshua.antiakainen@gmail.com>
2025-10-12 22:09:07 -0600

    Initial commit

---
+++ include/core/string.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "type_aliases.h"
+#include <cstdlib>
+
+struct StringBuilder;
+struct String;
+
+struct StringBuilder {
+	char *data;
+	usize length;
+	usize cap;
+
+	void append(char ch) {
+		if (length >= cap) {
+			cap *= 2;
+			data = (char *)realloc(data, cap);
+		}
+
+		data[length] = ch;
+		length++;
+	}
+
+	StringBuilder(usize capacity) {
+		data = (char *)malloc(sizeof(char) * capacity);
+		cap = capacity;
+		length = 0;
+	}
+};
+
+struct String
+{
+	const char *data;
+	usize length;
+
+	String(StringBuilder *sb)
+	{
+		data = sb->data;
+		length = sb->length;
+	}
+};
